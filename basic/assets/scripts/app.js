2279528476e79524309521aa9f03487e81b258b6
d_flex <hiyoo95@gmail.com>
2021-12-11 23:40:44 +0900

    chapter const and let, function

---
+++ basic/assets/scripts/app.js
@@ -0,0 +1,8 @@
+const defaultResult = 17;
+let currentResult = defaultResult;
+
+currentResult = currentResult * 3;
+
+let calculationDesc = `${defaultResult} * 3`;
+
+outputResult(currentResult, calculationDesc);
