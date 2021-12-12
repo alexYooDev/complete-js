97c7b512b8cb5539565f2025784801d9eac5a458
d_flex <hiyoo95@gmail.com>
2021-12-12 17:30:39 +0900

    function exapmple

--- basic/assets/scripts/app.js
+++ basic/assets/scripts/app.js
@@ -1,8 +1,18 @@
-const defaultResult = 17;
-let currentResult = defaultResult;
+const defaultResult = 39;
+let currentResult = defaultResult; // insert the value copy of the const into let
+let num = 3;
 
-currentResult = currentResult * 3;
+const add = (x, y) => {
+  const result = x + y;
+  return result;
+};
 
-let calculationDesc = `${defaultResult} * 3`;
+currentResult = add(currentResult, num);
+
+let calculationDesc = `${defaultResult} + ${num}`;
+//use template literal ; es6 syntax
+
+// let errorMessage = "'An error'" + ' occured!';
+// \n : line break <br>, ==> backslash(\) take the special traits away from the special character of a string (escape)
 
 outputResult(currentResult, calculationDesc);
